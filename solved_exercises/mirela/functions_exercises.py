58bbcfdb9eb87df8f03822116b92bb7d38eb89f9
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2021-02-19 20:54:05 +0200

    refactored function exercises

--- solved_exercises/mirela/functions_exercises.py
+++ solved_exercises/mirela/functions_exercises.py
@@ -3,15 +3,18 @@
 
 def calculate_factorial(x):
     """ Calculates the factorial of a number (positive integer) """
-    factorial = 1
+    if type(x) is not int:
+        raise TypeError("Expected a positive integer.")
+
     if x < 0:
-        return "Factorial can't be calculated for negative numbers "
-    elif x == 0:
-        return 1
-    else:
+        raise ValueError("Factorial function expected a positive integer.")
+
+    factorial = 1
+    if x > 0:
         for i in range(1, x + 1):
             factorial = factorial * i
-        return factorial
+
+    return factorial
 
 
 print(calculate_factorial(5))
@@ -19,18 +22,19 @@ print(calculate_factorial(5))
 
 def fibonacci(n):
     """ Generates a list with Fibonacci sequence up to a given n number. """
-    a = 0
-    b = 1
-    list = [a, b]
     if n < 0:
-        return "Please insert a positive integer "
-    elif n == 0:
-        return a
-    else:
-        while a + b < n:
+        raise ValueError("Please insert a positive integer.")
+
+    a, b = 0, 1
+    fibo_list = [a]
+
+    if n > 0:
+        while b <= n:
             a, b = b, a + b
-            list.append(b)
-        return list
+            fibo_list.append(a)
 
+    return fibo_list
 
-print(fibonacci(63))
+print(fibonacci(54))
+print(fibonacci(55))
+print(fibonacci(56))
