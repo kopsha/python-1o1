1219867bdc3de5b5f565db6b0236580276a960aa
GMDaniel <daniel.giura@gmail.com>
2021-02-22 17:30:19 +0200

    test all functions and update functions

--- solved_exercises/dani/04-functions.py
+++ solved_exercises/dani/04-functions.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 # Calculate the factorial of a number (positive integer).
 def factorial(n):
     if n > 0:
@@ -7,29 +9,27 @@ def factorial(n):
     return result
 
 
-value = int(input("Please enter a numeric value:\n"))
-print("Factorial of", value, "is", factorial(value))
-
 # Write a function that flattens a list.
 def flatten(raw_list):
     flat_list = []
-    for element in raw_list:
-        if type(element) == list:
-            flat_list.extend(flatten(element))
-        else:
-            flat_list.append(element)
+    if not isinstance(raw_list, list):
+        flat_list = []
+    else:
+        for element in raw_list:
+            if type(element) == list:
+                flat_list.extend(flatten(element))
+            else:
+                flat_list.append(element)
     return flat_list
 
 
-given_list = ["a", 13, [16], [56.5], [147], [18.2], [18.0, [14, 15]]]
-print("Flattened list", flatten(given_list))
-
 # Generate a list with Fibonacci sequence up to a number.
-from math import sqrt
 
 
 def fibonacci(n):
     result = [1]
+    if n < 1:
+        result = []
     for i in range(1, n + 1):
         if is_fibonaci(i):
             result.append(i)
@@ -49,19 +49,16 @@ def is_perfect_square(n):
     return False
 
 
-print("v1 Fibonacci list up to", value, "is", fibonacci(value))
-
-
-def fibonacci2(n):
+def fibonacci2(value):
     lista = [1, 1]
-    for i in range(2, value):
+    if value < 1:
+        lista = []
+    for i in range(2, value + 1):
         suma = lista[-1] + lista[-2]
         if suma <= value:
             lista.append(suma)
     return lista
 
-
-print("v2 Fibbonaci list up to", value, "is", fibonacci2(value))
 
 # Generate a list with all prime numbers less than a number.
 def primes(number):
@@ -79,4 +76,143 @@ def is_prime(number):
     return all_mod_multiplied > 0
 
 
-print("Primes list up to", value, "is", primes(value))
+def test_factorial():
+    """we'll test the correctnes of factorial function"""
+
+    print("Testing factorial() function...")
+
+    test_data = [
+        # (value, expected)
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+        (2, 2),
+        (9, 362880),
+    ]
+
+    fail_count = 0
+    for value, expected in test_data:
+        actual = factorial(value)
+        result = "passed" if actual == expected else "failed"
+
+        if result == "failed":
+            print(f"\t -> Actual {actual} is different from {expected}.")
+            fail_count += 1
+
+    total_count = len(test_data)
+    passed_count = total_count - fail_count
+    print(
+        f"Ran {total_count} tests out of {passed_count} were successful"
+        f" and {fail_count} did failed."
+    )
+
+
+def test_fibonnaci():
+    """we'll test the correctnes of fibonacci function"""
+
+    print("Testing fibonacci() function...")
+
+    test_data = [
+        # (value, expected)
+        (-1, []),
+        (0, []),
+        (1, [1, 1]),
+        (2, [1, 1, 2]),
+        (3, [1, 1, 2, 3]),
+        (15, [1, 1, 2, 3, 5, 8, 13]),
+    ]
+
+    fail_count = 0
+    for value, expected in test_data:
+        # actual = fibonacci2(value)
+        actual = fibonacci(value)
+        result = "passed" if actual == expected else "failed"
+
+        if result == "failed":
+            print(f"\t -> test {value} Actual {actual} is different from {expected}.")
+            fail_count += 1
+
+    total_count = len(test_data)
+    passed_count = total_count - fail_count
+    print(
+        f"Ran {total_count} tests out of {passed_count} were successful"
+        f" and {fail_count} did failed."
+    )
+
+
+def test_flatten():
+    """we'll test the correctnes of flatten function"""
+
+    print("Testing flatten() function...")
+
+    test_data = [
+        # (value, expected)
+        (
+            ["a", 13, [-16], [56.5], [147], [18.2], [18.0, [14, 15]]],
+            ["a", 13, -16, 56.5, 147, 18.2, 18.0, 14, 15],
+        ),
+        ([], []),
+        ([1, 2], [1, 2]),
+        (1, []),
+        ("abcd", []),
+    ]
+
+    fail_count = 0
+    for value, expected in test_data:
+        actual = flatten(value)
+        result = "passed" if actual == expected else "failed"
+
+        if result == "failed":
+            print(f"\t -> test Actual {actual} is different from {expected}.")
+            fail_count += 1
+
+    total_count = len(test_data)
+    passed_count = total_count - fail_count
+    print(
+        f"Ran {total_count} tests out of {passed_count} were successful"
+        f" and {fail_count} did failed."
+    )
+
+
+def test_primes():
+    """we'll test the correctnes of primes function"""
+
+    print("Testing primes() function...")
+
+    test_data = [
+        # (value, expected)
+        (-1, []),
+        (0, []),
+        (1, []),
+        (2, [2]),
+        (3, [2, 3]),
+        (15, [2, 3, 5, 7, 11, 13]),
+    ]
+
+    fail_count = 0
+    for value, expected in test_data:
+        # actual = fibonacci2(value)
+        actual = primes(value)
+        result = "passed" if actual == expected else "failed"
+
+        if result == "failed":
+            print(f"\t -> test {value} Actual {actual} is different from {expected}.")
+            fail_count += 1
+
+    total_count = len(test_data)
+    passed_count = total_count - fail_count
+    print(
+        f"Ran {total_count} tests out of {passed_count} were successful"
+        f" and {fail_count} did failed."
+    )
+
+
+def main():
+    # self checking part
+    test_factorial()
+    test_fibonnaci()
+    test_flatten()
+    test_primes()
+
+
+main()
