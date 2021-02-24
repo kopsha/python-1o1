2ce78f748621c1643cbea6b7640ce2ba958bc205
GMDaniel <daniel.giura@gmail.com>
2021-02-24 19:42:49 +0200

    optimize

--- solved_exercises/dani/05-prime_factorization.py
+++ solved_exercises/dani/05-prime_factorization.py
@@ -1,16 +1,18 @@
-def primes(number):
-    primes_list = []
-    for i in range(2, number + 1):
-        if is_prime(i):
-            primes_list.append(i)
-    return primes_list
+from math import sqrt
 
 
-def is_prime(number):
-    all_mod_multiplied = 1
-    for i in range(2, number):
-        all_mod_multiplied *= number % i
-    return all_mod_multiplied > 0
+def primes_fast(number):
+    dropped = set()
+    kept = set()
+    i = 2
+    while i <= number:
+        if i not in dropped:
+            kept.add(i)
+        dropped.update(set(range(i + i, number + 1, i)))
+        # print(i, "dropped ",dropped)
+        # print(i + i, limit, i)
+        i += 1
+    return kept
 
 
 def prime_factorization(number):
@@ -19,14 +21,16 @@ def prime_factorization(number):
     [(prime_1,prime_pow_1), prime_2,prime_pow_2), ... prime_n,prime_pow_n)]
     """
     factors = []
-    primes_list = primes(number)
-    for element in primes_list:
-        prime_pow = 0
-        while number % element == 0:
-            number /= element
-            prime_pow += 1
-        if prime_pow > 0:
-            factors.append((element, prime_pow))
+    primes = primes_fast(number)
+    # print("DEBUG",primes)
+    for prime in primes:
+        exponent = 0
+        # print(number % prime == 0, number, prime)
+        while number % prime == 0:
+            number //= prime
+            exponent += 1
+        if exponent > 0:
+            factors.append((prime, exponent))
     return factors
 
 
@@ -41,11 +45,17 @@ def test_prime_factorization():
         (420, [(2, 2), (3, 1), (5, 1), (7, 1)]),
         (1, []),
         (2, [(2, 1)]),
+        (11, [(11, 1)]),
         (4, [(2, 2)]),
-        # (
-        #    1111,
-        #    [(3, 1), (7, 1), (11, 1), (37, 1)],
-        # ),
+        (7, [(7, 1)]),
+        (15, [(3, 1), (5, 1)]),
+        (10999, [(17, 1), (647, 1)]),
+        (109999, [(317, 1), (347, 1)]),
+        (1099999, [(29, 1), (83, 1), (457, 1)]),
+        (
+            8547,
+            [(3, 1), (7, 1), (11, 1), (37, 1)],
+        ),
     ]
 
     fail_count = 0
@@ -67,6 +77,7 @@ def test_prime_factorization():
 
 def main():
     # self checking part
+    # primes_fast(7)
     test_prime_factorization()
 
 
