510a1d24a195790dd77a11c31ab74f4dd59c9464
GMDaniel <5613173+GMDaniel@users.noreply.github.com>
2021-02-25 21:43:13 +0200

    Optimize (#18)
    * optimize
    * added time tracking
    * ultra fast prime factorization
    * altceva
    * achieved PEP8 compliance
    
    Co-authored-by: Florin Ciurcanu <florin.ciurcanu@gmail.com>

--- solved_exercises/dani/prime_factorization.py
+++ solved_exercises/dani/prime_factorization.py
@@ -1,21 +1,7 @@
-from math import sqrt
+"""The fastest prime factorization I could come up with"""
 
 from datetime import datetime
 import timeit
-
-
-def primes_fast(number):
-    dropped = set()
-    kept = set()
-    i = 2
-    while i <= number:
-        if i not in dropped:
-            kept.add(i)
-        dropped.update(set(range(i + i, number + 1, i)))
-        # print(i, "dropped ",dropped)
-        # print(i + i, limit, i)
-        i += 1
-    return kept
 
 
 def prime_factorization(number):
@@ -24,16 +10,27 @@ def prime_factorization(number):
     [(prime_1,prime_pow_1), prime_2,prime_pow_2), ... prime_n,prime_pow_n)]
     """
     factors = []
-    primes = primes_fast(number)
-    # print("DEBUG",primes)
-    for prime in primes:
-        exponent = 0
-        # print(number % prime == 0, number, prime)
-        while number % prime == 0:
-            number //= prime
+    dividend = number
+    exponent = 0
+    is_prime = True
+
+    divisor = 2
+    while divisor <= number:
+        if (dividend % divisor) == 0:
+            dividend //= divisor
             exponent += 1
+            continue  # we want to attempt this divisor again
         if exponent > 0:
-            factors.append((prime, exponent))
+            factors.append((divisor, exponent))
+            exponent = 0
+            is_prime = False
+            if divisor > dividend:
+                break  # there is no need to explore further
+        divisor += 1
+
+    if is_prime:
+        factors.append((dividend, 1))
+
     return factors
 
 
@@ -46,18 +43,20 @@ def test_prime_factorization():
         # (value, expected)
         (140, [(2, 2), (5, 1), (7, 1)]),
         (420, [(2, 2), (3, 1), (5, 1), (7, 1)]),
-        (1, []),
         (2, [(2, 1)]),
         (11, [(11, 1)]),
+        (12, [(2, 2), (3, 1)]),
         (4, [(2, 2)]),
         (7, [(7, 1)]),
         (15, [(3, 1), (5, 1)]),
         (10_999, [(17, 1), (647, 1)]),
         (109_999, [(317, 1), (347, 1)]),
-        (10_099_999, [(29, 1), (83, 1), (457, 1)]),
+        (10_099_999, [(7, 1), (13, 1), (110989, 1)]),
+        (8547, [(3, 1), (7, 1), (11, 1), (37, 1)]),
+        (1_000_000_000_000, [(2, 12), (5, 12)]),
         (
-            8547,
-            [(3, 1), (7, 1), (11, 1), (37, 1)],
+            999_999_999_999,
+            [(3, 3), (7, 1), (11, 1), (13, 1), (37, 1), (101, 1), (9901, 1)],
         ),
     ]
 
@@ -79,11 +78,10 @@ def test_prime_factorization():
 
 
 def main():
-    # self checking part
-    # primes_fast(7)
+    """self checking part"""
     test_prime_factorization()
 
 
 duration = timeit.timeit(main, number=1)
-now = datetime.now().strftime('%H:%M:%S')
-print(f'[{now}] Finished in {duration:.2f} seconds.')
+now = datetime.now().strftime("%H:%M:%S")
+print(f"[{now}] Finished in {duration:.2f} seconds.")
