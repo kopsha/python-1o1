48ab3cb4befba879a29f09f5e2b61e3a371f285d
Marius Ungureanu <marius_ungureanu@epam.com>
2021-02-26 20:35:42 +0100

    update fu^ntions and add TODO

--- solved_exercises/marius/04-functions.py
+++ solved_exercises/marius/04-functions.py
@@ -4,13 +4,15 @@ import math
 
 def calc_factorial(n):
     """calculates factorial of a positive integer number - to be refactored"""
+    if not isinstance(n, int):
+        raise TypeError("Provided input number must be an integer.")
     if n <= 0:
-        return "Invalid input. Please provide positive integer"
-    if type(n) != int:
-        return "Provided input number must be an integer."
+        raise ValueError("Invalid input. Please provide positive integer")
     fact = 1
     for i in range(1, n + 1):
         fact *= i
+    # TODO add test functions
+    # values for test: 1,5,1000
     return fact
 
 
@@ -22,17 +24,20 @@ def calc_factorial(n):
 def fibonacci(n):
     """function generates Fibonacci list until a given input - to be refactored"""
     if n < 2:
-        return "Invalid number; input number must be at least equal to 2"
+        raise ValueError("Invalid number; input number must be at least equal to 2")
     a, b = 0, 1
     fib_list = [a, b]
     while b < n - a:
         a, b = b, a + b
         fib_list.append(b)
+    # TODO add tests
     return fib_list
 
-"""to discuss"""
+
+# TODO refactor and use classical recursive function
 flat = lambda given_list: [elem for item in given_list for elem in flat(item)] if isinstance(given_list, list) else [
     given_list]
+
 #print(flat([[1, 2, 3], [3, 6, 7], [7, 5, 4], 7]))
 
 
@@ -74,6 +79,38 @@ def test_get_primes():
     fail_count = 0
     for value, expected in test_data:
         actual = get_primes(value)
+        result = "passed" if actual == expected else "failed"
+
+        if result == "failed":
+            print(f"\t -> Actual {actual} is different from {expected}.")
+            fail_count += 1
+
+    total_count = len(test_data)
+    passed_count = total_count - fail_count
+    print(
+        f"Ran {total_count} tests out of with {passed_count} were successful"
+        f" and {fail_count} did failed."
+    )
+
+def test_flatten():
+    """we'll test the correctness of list_flatten function"""
+
+    print("Testing list_flatten() function...")
+
+    test_data = [
+        # (value, expected)
+        ([1, 6, [6, 32, "a", 17], 19, 17, 25, [20, 19]], [1, 6, 6, 32, "a", 17, 19, 17, 25, 20, 19]),
+        ([[0, 3], [0, 3], [19, 17, 25, [20, 19]]], [0, 3, 0, 3, 19, 17, 25, 20, 19]),
+        ([1, 5, 7], [1, 5, 7]),
+        ([], []),
+        ([[[1, 2, 3]]], [1, 2, 3]),
+        ([1, 2, 3, []], [1, 2, 3]),
+
+    ]
+
+    fail_count = 0
+    for value, expected in test_data:
+        actual = flatten(value)
         result = "passed" if actual == expected else "failed"
 
         if result == "failed":
