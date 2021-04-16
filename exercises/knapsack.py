a6d22cb50a4f26d93ceee3092ec0ab5f221a0cc7
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2021-04-16 22:22:48 +0300

    added all solutions

--- exercises/knapsack.py
+++ exercises/knapsack.py
@@ -12,7 +12,7 @@ list of items that were added to the bag (in the same order that they were
 given), the total weight of those items, and the total value of the items.
 
 Example
-knapsack(0, items) ➞ {
+knapsack(0, items) => {
   "capacity": 0,
   "items": [],
   "weight": 0,
@@ -20,12 +20,147 @@ knapsack(0, items) ➞ {
 }
 """
 
+from collections import namedtuple, deque
+from itertools import combinations
+from datetime import datetime
+import timeit
 
-def knapsack(capacity, items):
-    pass
+from pprint import pprint
+
+SackItem = namedtuple("SackItem", ["name", "weight", "value"])
 
 
-def test_knapsack():
+def iter_knapsack(capacity, items):
+    reduced_items = [SackItem(**i) for i in items if i["weight"] <= capacity]
+
+    best_value = 0
+    best_weight = 0
+    best_items = []
+
+    for k in reversed(range(1, len(reduced_items) + 1)):
+        for proposal in combinations(reduced_items, k):
+            p_weigth = sum(p.weight for p in proposal)
+            if p_weigth > capacity:
+                continue
+
+            p_value = sum(p.value for p in proposal)
+            if p_value > best_value:
+                best_value = p_value
+                best_weight = p_weigth
+                best_items = [p._asdict() for p in proposal]
+
+    return {
+        "capacity": capacity,
+        "items": best_items,
+        "weight": best_weight,
+        "value": best_value,
+    }
+
+
+
+def back_knapsack(capacity, all_items):
+    items = [SackItem(**i) for i in all_items if i["weight"] <= capacity]
+    n = len(items)
+
+    # print("--" * 20)
+    # print([(x["name"], x["weight"], x["value"]) for x in all_items])
+
+    best_value = 0
+    best_weight = 0
+    best_solution = []
+    remaining = tuple(range(n))
+
+    stack = deque([ ((), remaining) ])
+    # pprint(stack)
+
+    while stack:
+        # print(" >> stack")
+        # pprint(stack)
+        solution, moves = stack.popleft()
+        weight = sum(items[x].weight for x in solution)
+        left_capacity = capacity - weight
+
+        valid_moves = [m for m in moves if items[m].weight <= left_capacity]
+        for i, m in enumerate(valid_moves):
+            stack.append(( solution + (m,), tuple(valid_moves[i+1:]) ))
+
+
+        if not valid_moves:
+
+            value = sum(items[x].value for x in solution)
+            if value > best_value:
+                best_value = value
+                best_weight = weight
+                best_solution = solution
+                # print(" >> best", best_weight, best_value, [items[x].name for x in best_solution])
+
+    return {
+        "capacity": capacity,
+        "items": [items[x]._asdict() for x in best_solution],
+        "weight": best_weight,
+        "value": best_value,
+    }
+
+
+
+def recursive_knapsak(capacity, items):
+
+    max_value = 0
+    max_weight = 0
+    max_valued_packed = []
+
+    if len(items) == 0:
+        return max_value, max_valued_packed, max_weight
+
+    for i, item in enumerate(items):
+        if item["weight"] > capacity:
+            continue
+
+        value, packed, weight = recursive_knapsak(
+            capacity - item["weight"], items[i + 1 :]
+        )
+        if value + item["value"] >= max_value:
+            max_value = value + item   ["value"]
+            max_valued_packed = [item  ] + packed
+            max_weight = weight + item ["weight"]
+
+    return max_value, max_valued_packed, max_weight
+
+
+def recur_knapsack(capacity, items):
+    knapsack_items = []
+    items_weight = 0
+    items_value = 0
+
+    items_value, knapsack_items, items_weight = recursive_knapsak(capacity, items)
+
+    return {
+        "capacity": capacity,
+        "items": knapsack_items,
+        "weight": items_weight,
+        "value": items_value,
+    }
+
+
+def test_knapsack(knapsack):
+
+    items3 = [
+        {"name": "desk lamp", "weight": 1, "value": 1000},
+        {"name": "beach towel", "weight": 29, "value": 900},
+        {"name": "textbook", "weight": 1, "value": 899},
+        {"name": "wall clock", "weight": 1, "value": 850},
+    ]
+    assert knapsack(30, items3) == {
+        "capacity": 30,
+        "items": [
+            {"name": "desk lamp", "weight": 1, "value": 1000},
+            {"name": "textbook", "weight": 1, "value": 899},
+            {"name": "wall clock", "weight": 1, "value": 850},
+        ],
+        "weight": 3,
+        "value": 2749,
+    }
+
     items = [
         {"name": "desk lamp", "weight": 2, "value": 12},
         {"name": "beach towel", "weight": 1, "value": 10},
@@ -70,6 +205,7 @@ def test_knapsack():
         "weight": 5,
         "value": 2807,
     }
+
     assert knapsack(14, items) == {
         "capacity": 14,
         "items": [
@@ -141,19 +277,27 @@ def test_knapsack():
         "value": 2327,
     }
 
-    items3 = [
-        {"name": "desk lamp", "weight": 1, "value": 1000},
-        {"name": "beach towel", "weight": 29, "value": 900},
-        {"name": "textbook", "weight": 1, "value": 899},
-        {"name": "wall clock", "weight": 1, "value": 850},
-    ]
-    assert knapsack(30, items3) == {
-        "capacity": 30,
-        "items": [
-            {"name": "desk lamp", "weight": 1, "value": 1000},
-            {"name": "textbook", "weight": 1, "value": 899},
-            {"name": "wall clock", "weight": 1, "value": 850},
-        ],
-        "weight": 3,
-        "value": 2749,
-    }
+
+def main_iter():
+    test_knapsack(knapsack=iter_knapsack)
+
+def main_recur():
+    test_knapsack(knapsack=recur_knapsack)
+
+def main_back():
+    test_knapsack(knapsack=back_knapsack)
+
+
+
+if __name__ == '__main__':
+    duration = timeit.timeit(main_recur, number=100)
+    now = datetime.now().strftime("%H:%M:%S")
+    print(f"[{now}] Recursive finished in {duration:.2f} seconds.")
+
+    duration = timeit.timeit(main_iter, number=100)
+    now = datetime.now().strftime("%H:%M:%S")
+    print(f"[{now}] Iterative finished in {duration:.2f} seconds.")
+
+    duration = timeit.timeit(main_back, number=100)
+    now = datetime.now().strftime("%H:%M:%S")
+    print(f"[{now}] Backtracking finished in {duration:.2f} seconds.")
