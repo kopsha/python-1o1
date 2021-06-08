84f1982bf4acfd235dcafe3ae7abcc98c3413301
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2021-06-08 11:36:52 +0300

    added reverse words exercise

---
+++ exercises/reverse_words.py
@@ -0,0 +1,24 @@
+"""
+Level 1: Reverse each word of a string:
+^^^^^^^
+
+>>> given = "Hi my name is Jessica"
+>>> expected = "iH ym eman si acisseJ"
+
+Level 2: Reverse all words from a given text file
+^^^^^^^
+
+given.txt:
+Structured programming is a programming paradigm aimed at improving the clarity
+quality and development time of a computer program by making extensive use of
+the structured control flow constructs of selection if then else and repetition
+while and for block structures and subroutines
+
+expected.txt:
+derutcurtS gnimmargorp si a gnimmargorp mgidarap demia ta gnivorpmi eht ytiralc
+ytilauq dna tnempoleved emit fo a retupmoc margorp yb gnikam evisnetxe esu fo
+eht derutcurts lortnoc wolf stcurtsnoc fo noitceles fi neht esle dna noititeper
+elihw dna rof kcolb serutcurts dna senituorbus
+"""
+
+# your code goes here.
